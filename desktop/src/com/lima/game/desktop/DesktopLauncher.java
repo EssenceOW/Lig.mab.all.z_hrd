c1a4d916a61df91c6206d3b7a746f6956f4e5c7a
EssenceOW <22michaell@gatewayhigh.net>
2019-07-09 10:13:52 -0700

    create project

---
+++ desktop/src/com/lima/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lima.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lima.game.blzhrd;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new blzhrd(), config);
+	}
+}
